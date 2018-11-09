f69e3d45e9208d404310fdf11e91c7039e336e9e
Jin Hang <jinhang@nuaa.edu.cn>
2018-11-09 14:53:10 +0800

    repaired setting cookies

---
+++ files/common.js
@@ -0,0 +1,50 @@
+
+window.onload = function(){
+  var verify = new Vue({
+    el:'#verify',
+    data:{
+      loginuser : ''
+    },
+    methods:{
+      logout:function(){
+        //发送 post 请求
+        this.$http.get('http://localhost:8000/logout/', {credentials: true}).then(function(res){
+          //document.write(res.bodyText);  
+          console.log(res.bodyText);
+          var dataret = JSON.parse(res.bodyText);
+          if (dataret.code == 200)
+          {
+            console.log(dataret.redirect);
+            window.location.href = 'index.html';
+          }
+          else
+          {
+            alert('登录错误！');
+          }
+          //console.log(res);
+          //console.log(JSON.parse(res.bodyText));
+          //window.location.href='http://www.ddhbb.com';
+        },function(res){
+          console.log(res.status);
+        });
+      }
+    },
+    created:function(){
+      this.$http.get('http://localhost:8000/login/', {credentials: true}).then(function(res){
+        console.log(res.bodyText);
+        var dataret = JSON.parse(res.bodyText);
+        if (dataret.code == 200)
+        {
+          this.loginuser = dataret.username;
+        }
+        else
+        {
+          this.loginuser = dataret.info;
+          window.location.href = 'error.html';
+        }
+      },function(res){
+        console.log(res.status);
+      });
+    }
+  })
+}
