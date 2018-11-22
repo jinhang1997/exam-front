294a8ab194efd821653549358c6565eef89b0dd9
Jin Hang <jinhang@nuaa.edu.cn>
2018-11-22 22:34:52 +0800

    add showing paper list and showing details

--- files/common.js
+++ files/common.js
@@ -48,3 +48,10 @@ window.onload = function(){
     }
   })
 }
+
+function getQueryString(name) { 
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+  var r = window.location.search.substr(1).match(reg); 
+  if (r != null) return unescape(r[2]); 
+  return null; 
+} 
