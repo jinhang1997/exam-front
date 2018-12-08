a8e3d0222862324160e6f08ed7c17d6de797be07
Jin Hang <jinhang@nuaa.edu.cn>
2018-12-09 00:58:15 +0800

    able to show testinfo on testing,html fixed sending answers with papername

--- files/testing.js
+++ files/testing.js
@@ -2,24 +2,28 @@
 var vm = new Vue({
   el:'#show_paper',
   data:{
-    paperid: '',
-    test: ''
-    /*test:{
+    paper: '',
+    test:{
       test_problem: [
         { id: '1',problem: '1+1=?',type: 'keguan',point: '5',option1: '5',option2: '2',option3: '4',option4: '3',answer: '',},
         { id: '2',problem: '你好吗？',type: 'zhuguan',point: '10',option1: '',option2: '',option3: '',option4: '',answer: '',}
       ]
-    }*/
+    }
   },
   methods:{
     submit_answer:function(){
-      this.$http.post(backend_server + 'test-manage/', this.test, {credentials: true})
+      postdata = {
+        paperid: this.paper.pid,
+        pname: this.paper.pname,
+        test: this.test
+      };
+      this.$http.post(backend_server + 'test-manage/', postdata, {credentials: true})
       .then(function(res){
         console.log(res.bodyText);
         var dataret = JSON.parse(res.bodyText);
         if (dataret.code == 200)
         {
-          alert('提交答案成功：用户' + dataret.stu);
+          alert('提交答案成功：' + dataret.stu + '的试卷' + dataret.pname);
         }
         else
         {
@@ -31,13 +35,14 @@ var vm = new Vue({
       });
     },
     get_test:function(){
-      this.$http.get(backend_server + 'test-manage/?id=' + this.paperid, {credentials: true})
+      this.$http.get(backend_server + 'test-manage/?paperid=' + this.paperid, {credentials: true})
       .then(function(res){
         console.log(res.bodyText);
         var dataret = JSON.parse(res.bodyText);
         if (dataret.code == 200)
         {
           this.test = dataret.test;
+          this.paper = dataret.test_info;
         }
         else
         {
